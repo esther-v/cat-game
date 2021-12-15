7631e25e5d4148566304308112f985f8dffff60c
esther-v <esvalas@gmail.com>
2021-12-15 21:36:50 +0100

    cat moves ok 🐈‍⬛

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const WORLD_HEIGHT = 30
 const SPEED_SCALE_INCREASE = .00001
 const worldEl = document.querySelector("[data-world]")
 const groundElems = document.querySelectorAll("[data-ground]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
+
 
 let lastTime
 let speedScale
@@ -22,17 +25,19 @@ function setPixelToWorldScale(){
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale)
 
-
 //start the game
 document.addEventListener("keydown", handleStart, {once: true})
+
 function handleStart() {
     lastTime = null
     speedScale = 1
     score = 0
     setupGround()
+    setupCat() 
+    setupCactus()
+    startScreenElem.classList.add("hide")
     window.requestAnimationFrame(update)
 }
-
 
 //update custom property
 function getCustomProperty(elem, prop) {
@@ -47,7 +52,7 @@ function incrementCustomProperty(elem, prop, inc) {
     setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
 }
 
-//update the game depending on time
+//update the game, make the moves
 function update(time) {
     if(lastTime == null) {
         lastTime = time
@@ -57,6 +62,8 @@ function update(time) {
     const delta = time - lastTime
 
     updateGround(delta, speedScale)
+    updateCat(delta, speedScale)
+    updateCactus(delta, speedScale)
     updateSpeedScale(delta)
     updateScore(delta)
 
@@ -68,7 +75,10 @@ function updateSpeedScale(delta) {
     speedScale += delta * SPEED_SCALE_INCREASE
 }
 
-
+function updateScore(delta) {
+    score += delta * 0.01 
+    scoreElem.textContent = Math.floor(score)
+}
 
 
 //update ground
@@ -87,5 +97,68 @@ function updateGround(delta, speedScale) {
             incrementCustomProperty(ground, "--left", 600)
         }
     })
+}
+
+
+
+//update cat moves
+const catElem = document.querySelector('[data-cat]')
+const JUMP_SPEED = .45
+const GRAVITY = .0015
+const CAT_FRAME_COUNT = 2
+const FRAME_TIME = 100
+
+let isJumping
+let catFrame
+let currentFrameTime
+let yVelocity
+
+function setupCat(){
+    isJumping = false
+    catFrame = 0
+    currentFrameTime = 0
+    yVelocity = 0
+    setCustomProperty(catElem, "--bottom", 0)
+    document.removeEventListener("keydown", onJump)
+    document.addEventListener("keydown", onJump)
+}
+
+function updateCat(delta, speedScale){
+    handleRun(delta, speedScale)
+    handleJump(delta)
+}
+
+function handleRun(delta, speedScale) {
+    if(isJumping) {
+        catElem.src = `img/cat-attack.png`
+        return
+    }
+
+    if (currentFrameTime >= FRAME_TIME) {
+        catFrame = (catFrame + 1) % CAT_FRAME_COUNT
+        catElem.src = `img/cat-run-${catFrame}.png` 
+        currentFrameTime -= FRAME_TIME   
+    }
+    currentFrameTime += delta * speedScale
+}
+
+function handleJump(delta) {
+    if(!isJumping) return
+
+    incrementCustomProperty(catElem, "--bottom", yVelocity * delta)
+    
+    if(getCustomProperty(catElem, "--bottom") <= 0) {
+        setCustomProperty(catElem, "--bottom", 0)
+        isJumping = false
+    }
+
+    yVelocity -= GRAVITY * delta
+}
+
+function  onJump(e) {
+    if (e.code !== "Space" || isJumping) return
+
+    yVelocity = JUMP_SPEED
+    isJumping = true
 }
 
