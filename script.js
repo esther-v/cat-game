9846a37fbf6d261d390c1f4cd0393f1c3da8e085
esther-v <esvalas@gmail.com>
2021-12-15 21:54:57 +0100

    starting update cactus

--- script.js
+++ script.js
@@ -162,3 +162,36 @@ function  onJump(e) {
     isJumping = true
 }
 
+//update cactus
+const CACTUS_INTERVAL_MIN = 500
+const CACTUS_INTERVAL_MAX = 2000
+let nextCactusTime
+
+function setupCactus() {
+    nextCactusTime = CACTUS_INTERVAL_MIN
+}
+
+function updateCactus(delta, speedScale) {
+    document.querySelectorAll("[data-cactus]").forEach(cactus => {
+        incrementCustomProperty(cactus, "--left", delta * speedScale * SPEED * -1)
+    })
+    if (nextCactusTime <= 0) {
+        createCactus()
+        nextCactusTime = randomNumberBetween(CACTUS_INTERVAL_MIN, CACTUS_INTERVAL_MAX) / speedScale
+    }
+    nextCactusTime -= delta
+}
+
+function createCactus() {
+    const cactus = document.createElement("img")
+    cactus.dataset.cactus = true
+    cactus.src = "img/cactus.png"
+    cactus.classList.add("cactus")
+    setCustomProperty(cactus, "--left", 100)
+    worldEl.append(cactus)
+}
+
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
