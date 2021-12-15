148bcc79e4b7fe178d81eb0d621d501e3ddac433
esther-v <esvalas@gmail.com>
2021-12-15 16:46:09 +0100

    create update custom property functions

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
-
 const worldEl = document.querySelector("[data-world]")
+const groundElems = document.querySelectorAll("[data-ground]")
 
+//resize the world depending on the screen size
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale)
 
@@ -15,4 +16,43 @@ function setPixelToWorldScale(){
     }
     worldEl.style.width = `${WORLD_WIDTH * worldToPixelScale}px` 
     worldEl.style.height = `${WORLD_HEIGHT * worldToPixelScale}px` 
+}
+
+//update depending on the time
+let lastTime
+
+function update(time) {
+    if(lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+
+    updateGround(delta)
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+
+window.requestAnimationFrame(update)
+
+//update ground
+function updateGround(delta) {
+    groundElems.forEach(ground => {
+
+    })
+}
+
+//update custom property
+function getCustomProperty(elem, prop) {
+    return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
+}
+
+function setCustomProperty() {
+
+}
+
+function incrementCustomProperty() {
+
 }
